49ef417ffb95bade2cd9228b03d750746141d496
MyriamBen <benhadjsghaier.my@Host-004.lan>
2025-10-27 12:52:59 +0100

    Dernière MAJ du script et du tableau TSV pour le comptage des mots avec User Agent

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -15,6 +15,8 @@ if [[ ! -r "$FICHIER" ]]; then
 	exit 1
 fi
 
+UserAgent="PPE1-Script/1.0 (INALCO-SORBONNE-NOUVELLE-2025; projet pédagogique 2025)"
+
 NUM_LIGNE=0
 
 
@@ -24,27 +26,29 @@ echo "--------------------------------------------------------------------------
 #for URL in "$FICHIER" > boucle for utile si plusieurs fichiers
 #do
 
-	while read -r LINE;
-	do
-		if [[ $LINE =~ ^https?:// ]]
-		then
-			HEADERS=$(curl -I -s --max-time 15 "$LINE" | tr -d '\r')
+	while read -r LINE; do
+	[[ $LINE =~ ^https?:// ]] || continue
 
-			CODE_HTTP=$(echo "$HEADERS" | head -n 1 | awk '{print $2}')
+		HEADERS=$(curl -IL -s --max-time 15 -A "$UserAgent" "$LINE" | tr -d '\r')
 
-			if echo "$HEADERS" | grep -qi "charset=";then
-				ENCODAGE=$(echo "$HEADERS" | grep -i "charset="| head -n 1| cut -d= -f2 | cut -d ';' -f1)
-			else
-				ENCODAGE="Pas d'encodage"
-			fi
+		CODE_HTTP=$(echo "$HEADERS" | head -n 1 | awk '{print $2}')
+		if [[ -z "$CODE_HTTP" ]]; then
+				CODE_HTTP="None"
+		fi
+
+		if echo "$HEADERS" | grep -qi "charset=";then
+			ENCODAGE=$(echo "$HEADERS" | grep -i "charset="| head -n 1| cut -d= -f2 | cut -d ';' -f1)
+		else
+			ENCODAGE="Pas d'encodage"
+		fi
+
+		CONTENU=$(curl -sL --compressed --max-time 30 -A "$UserAgent" "$LINE")
+		NB_MOTS=$(wc -w <<< "$CONTENU" | awk '{print $1}')
+		[[ -z "$NB_MOTS" ]] && NB_MOTS=0
 
 		NUM_LIGNE=$(($NUM_LIGNE + 1))
-		CONTENU=$(curl -s --max-time 30 "$LINE")
-		NB_MOTS=$(wc -w <<< "$CONTENU")
-		[[ -z "$NB_MOTS" ]] && NB_MOTS=0
 		echo -e "ligne $NUM_LIGNE\t$LINE\t$CODE_HTTP\t$ENCODAGE\t$NB_MOTS"
 
-		fi
 	done < "$FICHIER"
 
 #Si boucle for : done < "$URL"
